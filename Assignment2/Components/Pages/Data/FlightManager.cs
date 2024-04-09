46d8990815f2d4f11b54af9d18ad7860ba6462e1
sagar442-debug <bnradlyp1@gmail.com>
2024-04-08 19:59:42 -0600

    Completed the assignment

---
+++ Assignment2/Components/Pages/Data/FlightManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Assignment2.Components.Pages.Data
+{
+    internal class FlightManager
+    {
+        public static string WEEKDAY_ANY = "Any";
+        public static string WEEKDAY_SUNDAY = "Sunday";
+        public static string WEEKDAY_MONDAY = "Monday";
+        public static string WEEKDAY_TUESDAY = "Tuesday";
+        public static string WEEKDAY_WEDNESDAY = "Wednesday";
+        public static string WEEKDAY_THURSDAY = "Thursday";
+        public static string WEEKDAY_FRIDAY = "Friday";
+        public static string WEEKDAY_SATURDAY = "Saturday";
+
+        public static string FLIGHTS_TEXT = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\Resources\Files\flights.csv");
+        public static string AIRPORTS_TEXT = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\Resources\Files\airports.csv");
+
+        public static List<Flight> Flights { get; private set; }
+        public static List<string> airports { get; private set; }
+
+        public FlightManager()
+        {
+            Flights = new List<Flight>();
+            airports = new List<string>();
+            PopulateAirports();
+            PopulateFlights();
+        }
+
+        public string findAirportByCode(string code)
+        {
+            return airports.FirstOrDefault(airport => airport.Equals(code));
+        }
+
+        public static Flight findFlightByCode(string code)
+        {
+            return Flights.FirstOrDefault(flight => flight.Code.Equals(code));
+        }
+
+        public static List<Flight> findFlights(string from, string to, string weekday)
+        {
+            return Flights.Where(flight =>
+                (from == WEEKDAY_ANY || flight.From.Equals(from)) &&
+                (to == WEEKDAY_ANY || flight.To.Equals(to)) &&
+                (weekday == WEEKDAY_ANY || flight.Weekday.Equals(weekday))).ToList();
+        }
+
+        private void PopulateFlights()
+        {
+            try
+            {
+                foreach (string line in File.ReadLines(FLIGHTS_TEXT).Skip(1))
+                {
+                    string[] parts = line.Split(",");
+                    string code = parts[0];
+                    string airline = parts[1];
+                    string from = parts[2];
+                    string to = parts[3];
+                    string weekday = parts[4];
+                    string time = parts[5];
+                    int seatsAvailable = int.Parse(parts[6]);
+                    double pricePerSeat = double.Parse(parts[7]);
+                    Flights.Add(new Flight(code, airline, from, to, weekday, time, seatsAvailable, pricePerSeat));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading flights file: " + e.Message);
+            }
+        }
+
+        private void PopulateAirports()
+        {
+            try
+            {
+                foreach (string line in File.ReadLines(AIRPORTS_TEXT))
+                {
+                    string[] parts = line.Split(",");
+                    string code = parts[0];
+                    airports.Add(code);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error reading airports file: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Assignment2/Components/Pages/Data/ReservationManager.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Assignment2.Components.Pages.Data
+{
+    internal class ReservationManager
+    {
+        private static string Reservation_TXT = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\..\Resources\Files\reservation.csv");
+
+        private static Random random = new Random();
+
+        private static List<Reservation> reservations = new List<Reservation>();
+
+        public List<Reservation> FindReservations(string reservationCode, string airline, string name)
+        {
+            List<Reservation> found = new List<Reservation>();
+
+            foreach (Reservation reservation in reservations)
+            {
+                if (reservation.Code.Contains(reservationCode) && reservation.Airline.Contains(airline) && reservation.Name.Contains(name))
+                {
+                    found.Add(reservation);
+                }
+                else if (reservation.Code.Contains(reservationCode))
+                {
+                    found.Add(reservation);
+                }
+                else if (reservation.Airline.Contains(airline))
+                {
+                    found.Add(reservation);
+                }
+                else if (reservation.Name.Contains(name))
+                {
+                    found.Add(reservation);
+                }
+            }
+
+            return found;
+        }
+
+        public string GenerateResCode()
+        {
+            return GenerateReservationCode();
+        }
+
+        public string GenerateReservationCode()
+        {
+            string reservationCode;
+
+            do
+            {
+                char letter = (char)('A' + random.Next(26));
+                string numbers = random.Next(1000, 10000).ToString();
+                reservationCode = letter + numbers;
+            } while (IsCodeGenerated(reservationCode, Reservation_TXT));
+
+            return reservationCode;
+        }
+
+        private static bool IsCodeGenerated(string reservationCode, string Reservation_TXT)
+        {
+            if (!File.Exists(Reservation_TXT))
+            {
+                return false;
+            }
+
+            List<string> existingCode = File.ReadAllLines(Reservation_TXT).ToList();
+
+            return existingCode.Contains(reservationCode);
+        }
+
+        public static List<Reservation> GetReservations()
+        {
+            List<Reservation> res = new List<Reservation>();
+            foreach (string line in File.ReadLines(Reservation_TXT))
+            {
+                string[] parts = line.Split(",");
+                string reservationCode = parts[0];
+                string flightCode = parts[1];
+                string airline = parts[2];
+                double cost = double.Parse(parts[3]);
+                string name = parts[4];
+                string citizenship = parts[5];
+                string status = parts[6];
+
+                Reservation newReservation = new Reservation(reservationCode, flightCode, airline, cost, name, citizenship, status);
+                res.Add(newReservation);
+            }
+            reservations = res;
+            return res;
+        }
+
+        public void AddReservation(Reservation res)
+        {
+            File.AppendAllText(Reservation_TXT, $"{res.Code},{res.FlightCode},{res.Airline},{res.Cost},{res.Name},{res.Citizenship},{res.Active}\n");
+        }
+
+        public void UpdateReservation(Reservation res)
+        {
+            var lines = File.ReadAllLines(Reservation_TXT).ToList();
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                string[] parts = lines[i].Split(",");
+                if (parts[0].Equals(res.Code))
+                {
+                    parts[6] = res.Active;
+                    lines[i] = string.Join(",", parts);
+                    break;
+                }
+            }
+
+            File.WriteAllLines(Reservation_TXT, lines);
+        }
+    }
+}
